92b165fd9b981eaaa0a81834e6a1a25fc24c8aec
Askaraks <askar.mamadaliev@gmail.com>
2022-11-27 22:57:26 +0600

    переделал

--- app/src/main/java/com/example/loginuserh6mth2/MainActivity.java
+++ app/src/main/java/com/example/loginuserh6mth2/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.loginuserh6mth2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private EditText editEmail, editPassword;
+    private TextView come, hello,forgot,hello2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.bottom1);
+        editEmail = findViewById(R.id.editText2);
+        editPassword = findViewById(R.id.editText1);
+        come= findViewById(R.id.text4);
+        hello=findViewById(R.id.text2);
+        hello2 = findViewById(R.id.text1);
+        forgot = findViewById(R.id.text5);
+
+        button.setOnClickListener(view -> {
+            if (editEmail.getText().toString().equals("admin")&&
+                    editPassword.getText().toString().equals("admin")){
+                button.setVisibility(View.GONE);
+                editEmail.setVisibility(View.GONE);
+                editPassword.setVisibility(View.GONE);
+                come.setVisibility(View.GONE);
+                hello.setVisibility(View.GONE);
+                hello2.setVisibility(View.GONE);
+                forgot.setVisibility(View.GONE);
+
+
+            }else {
+                Toast.makeText(MainActivity.this, "Вы ввели неправильный пароль или " +
+                        "логин", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
